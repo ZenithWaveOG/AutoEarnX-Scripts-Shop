74804955d6ee7f0924413e649cc5941f59d14710
ZenithWaveOG <versegagan@gmail.com>
2026-03-14 16:50:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -468,6 +468,7 @@ async def main():
         app.run_polling()
 
 
-if __name__=="__main__":
-
-    asyncio.run(main())
+if __name__ == "__main__":
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
